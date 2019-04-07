7d8b0537fc033465d61130c8c085635e4a98f659
Wilfredo <wilfredo.paredes55@gmail.com>
2019-04-07 15:59:11 -0400

    *updated

--- menu.cpp
+++ menu.cpp
@@ -4,4 +4,5 @@ void menu(){
   cout << "\t\t\t Menu \t\t\t " << endl;
   cout << "1) Dada una fecha imprimir el numero de reportes abiertos" << endl;
   cout << "2) Dada una fecha imprimir el numero de reportes resueltos" << endl;
+  cout << "3) Dada una fecha imprimir el numero de reportes no resueltos" << endl;
 }
