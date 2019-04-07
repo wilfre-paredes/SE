b5815b5170c77baf672c3703cc25a03ba36c516b
Wilfredo <wilfredo.paredes55@gmail.com>
2019-04-07 15:56:48 -0400

    *updated

--- menu.cpp
+++ menu.cpp
@@ -2,5 +2,6 @@
 #include <iostream>
 void menu(){
   cout << "\t\t\t Menu \t\t\t " << endl;
-  cout << "1) Dada una fecha imprimir numero de reportes abiertos" << endl;
+  cout << "1) Dada una fecha imprimir el numero de reportes abiertos" << endl;
+  cout << "2) Dada una fecha imprimir el numero de reportes resueltos" << endl;
 }
